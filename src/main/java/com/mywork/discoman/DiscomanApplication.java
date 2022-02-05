f7013465934df420a78654a7e96281e20a1ae390
markkang05 <markkang05@markkang05s-MacBook-Air.local>
2022-02-05 23:32:33 +0900

    first Commit

---
+++ src/main/java/com/mywork/discoman/DiscomanApplication.java
@@ -0,0 +1,13 @@
+package com.mywork.discoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscomanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscomanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mywork/discoman/DiscomanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywork.discoman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscomanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
